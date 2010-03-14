869fe37cd25637958cbc846af3e3d34d1a47c09b
Jannis Leidel <jannis@leidel.info>
2010-03-14 23:18:17 +0100

    Updated setup.py file a little to behave nicer

--- setup.py
+++ setup.py
@@ -1,27 +1,11 @@
-# make sure setuptools is available 
-from ez_setup import use_setuptools
-use_setuptools()
-
 from setuptools import setup, find_packages
 import os
 
 version = __import__('image_filer').__version__
 
-media_files = []
-for dir in ['image_filer/media','image_filer/templates']:
-    for dirpath, dirnames, filenames in os.walk(dir):
-        media_files.append([dirpath, [os.path.join(dirpath, f) for f in filenames]])
-
 def read(fname):
     # read the contents of a text file
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
-
-install_requires = [
-    'setuptools',
-    'sorl-thumbnail>=3.2.5',
-    'django-mptt>=0.2.1',
-    'django>=1.1',
-]
 
 setup(
     name = "django-image-filer",
@@ -34,14 +18,24 @@ setup(
     author = 'Stefan Foulis',
     author_email = 'stefan.foulis@gmail.com',
     packages=find_packages(exclude=['ez_setup']),
-    install_requires = install_requires,
+    install_requires = [
+        'sorl-thumbnail>=3.2.5',
+        'django>=1.1',
+    ],
     package_data={
         '': ['*.txt', '*.rst',],
+        'image_filer': [
+            'media/css/*.css',
+            'media/flash/*.swf',
+            'media/icons/*.png',
+            'media/img/*',
+            'media/js/*',
+            'media/slideshow2/*/*',
+            'templates/*/*.html',
+            'templates/*/*/*.html',
+            'templates/*/*/*/*.html',
+        ],
     },
-    package_dir = {
-        'image_filer':'image_filer',
-    },
-    data_files = media_files,
     zip_safe=False,
     classifiers = [
         'Development Status :: 4 - Beta',
